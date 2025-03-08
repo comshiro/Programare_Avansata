d1a1b3a3a2e09ee7fd73d6e93962d82795df794f
comshiro <gheonea.naomidenisa@gmail.com>
2025-03-08 08:25:49 +0200

    added requirements'

--- lab1/src/main/java/pa_lab1/lab1/Lab1.java
+++ lab1/src/main/java/pa_lab1/lab1/Lab1.java
@@ -6,6 +6,19 @@ package pa_lab1.lab1;
 
 /**
  *
+ * Write a Java application that implements the following operations:
+
+Display on the screen the message "Hello World!". Run the application. If it works, go to step 2 :)
+Define an array of strings languages, containing {"C", "C++", "C#", "Python", "Go", "Rust", "JavaScript", "PHP", "Swift", "Java"}
+Generate a random integer n: int n = (int) (Math.random() * 1_000_000);
+Compute the result obtained after performing the following calculations:
+  multiply n by 3;
+  add the binary number 10101 to the result;
+  add the hexadecimal number FF to the result;
+  multiply the result by 6;
+Compute the sum of the digits in the result obtained in the previous step. This is the new result. While the new result has more than one digit, continue to sum the digits of the result.
+Display on the screen the message: "Willy-nilly, this semester I will learn " + languages[result].
+* 
  * @author Naomi
  */
 public class Lab1 {
@@ -35,8 +48,7 @@ public class Lab1 {
         System.out.println("Willy-nilly, this semester I will learn");
         System.out.println(languages[n]); ///  System.out.println(languages);
 
-        System.out.print("Willy-nilly, this semester I will learn "); 
-        System.out.print(languages[n]); ///  System.out.println(languages);
+        System.out.print("Willy-nilly, this semester I will learn " + languages[n]); 
 
     }
 }
