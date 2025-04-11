c75368fc2448e6406510c11cbe38f3d069304ba7
comshiro <gheonea.naomidenisa@gmail.com>
2025-04-11 19:20:45 +0300

    started bonus

--- lab6/src/main/java/org/example/lab6/MST.java
+++ lab6/src/main/java/org/example/lab6/MST.java
@@ -5,12 +5,10 @@ import java.util.*;
 
 public class MST {
 
-    // Calculează distanța euclidiană între două puncte
     public static double calculateDistance(Point p1, Point p2) {
         return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
     }
 
-    // Algoritmul lui Prim pentru calcularea MST-ului
     public static double calculateMST(List<Point> dots) {
         int n = dots.size();
         if (n <= 1) return 0;
@@ -44,8 +42,92 @@ public class MST {
         return totalWeight;
     }
 
-    // Clasă internă pentru a reprezenta o muchie (linie)
-    private static class Edge {
+    public static List<List<Edge>> generateSpanningTrees(List<Point> dots, int numTrees) {
+        List<List<Edge>> spanningTrees = new ArrayList<>();
+        int n = dots.size();
+
+        // Generate multiple spanning trees (starting from different nodes)
+        for (int i = 0; i < numTrees; i++) {
+            List<Edge> mst = generateMSTFromRandomStart(dots);
+            spanningTrees.add(mst);
+        }
+
+        return spanningTrees;
+    }
+
+    private static List<Edge> generateMSTFromRandomStart(List<Point> dots) {
+        int n = dots.size();
+        boolean[] inMST = new boolean[n];
+        List<Edge> mst = new ArrayList<>();
+        PriorityQueue<Edge> pq = new PriorityQueue<>(Comparator.comparingDouble(e -> e.weight));
+
+        // Start from a random node
+        Random random = new Random();
+        int startNode = random.nextInt(n);
+        inMST[startNode] = true;
+
+        for (int i = 0; i < n; i++) {
+            if (i != startNode) {
+                pq.add(new Edge(startNode, i, calculateDistance(dots.get(startNode), dots.get(i))));
+            }
+        }
+        while (!pq.isEmpty()) {
+            Edge edge = pq.poll();
+            if (!inMST[edge.v]) {
+                inMST[edge.v] = true;
+                mst.add(edge);
+
+                // Add edges from the newly added vertex
+                for (int i = 0; i < n; i++) {
+                    if (!inMST[i]) {
+                        pq.add(new Edge(edge.v, i, calculateDistance(dots.get(edge.v), dots.get(i))));
+                    }
+                }
+            }
+        }
+
+        return mst;
+    }
+
+    public static double calculateTotalWeight(List<Edge> edges, List<Point> dots) {
+        double totalWeight = 0;
+        for (Edge edge : edges) {
+            totalWeight += edge.weight;
+        }
+        return totalWeight;
+    }
+
+    public static List<Edge> getBestSpanningTree(List<List<Edge>> spanningTrees, List<Point> dots) {
+        List<Edge> bestTree = null;
+        double minCost = Double.MAX_VALUE;
+
+        for (List<Edge> tree : spanningTrees) {
+            double cost = calculateTotalWeight(tree, dots);
+            if (cost < minCost) {
+                minCost = cost;
+                bestTree = tree;
+            }
+        }
+
+        return bestTree;
+    }
+
+    public static List<Edge> getWorstSpanningTree(List<List<Edge>> spanningTrees, List<Point> dots) {
+        List<Edge> worstTree = null;
+        double maxCost = Double.MIN_VALUE;
+
+        for (List<Edge> tree : spanningTrees) {
+            double cost = calculateTotalWeight(tree, dots);
+            if (cost > maxCost) {
+                maxCost = cost;
+                worstTree = tree;
+            }
+        }
+
+        return worstTree;
+    }
+
+    static class Edge {
         int u, v;
         double weight;
 
